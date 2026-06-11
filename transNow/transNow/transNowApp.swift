9e3a17b70a9552b4074afddf9e945db57446eed5
ha ha <hahatestiphone2017@gmail.com>
2026-06-11 15:52:46 +0800

    Initial commit

---
+++ transNow/transNow/transNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  transNowApp.swift
+//  transNow
+//
+//  Created by Mike.Kuo on 2026/6/11.
+//
+
+import SwiftUI
+
+@main
+struct transNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ transNow/transNowUITests/transNowUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  transNowUITestsLaunchTests.swift
+//  transNowUITests
+//
+//  Created by Mike.Kuo on 2026/6/11.
+//
+
+import XCTest
+
+final class transNowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
